e2ccbb007c774ba44588a014b679bc7c3b9737d3
Ravi yadav <ry678279@gmail.com>
2025-11-02 07:14:57 +0530

    Add files via upload

---
+++ chapter1/sumtwonumber.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number1: "))
+b = int(input("Enter a number2: "))
+print("Sum of numbers:", a + b)
+# print("Sum of numbers:", a - b)#single line comment
+'''print("Sum of numbers:", a * b)
+print("Sum of numbers:", a / b)
+print("Sum of numbers:", a % b)''' #multi line comment
