864194bd8c37d7fc5ea9c3aa77e82179936c4cfa
Ravi yadav <ry678279@gmail.com>
2025-11-05 09:51:50 +0530

    Add program to sum two user-input numbers

---
+++ Practice2/problem01.py
@@ -0,0 +1,6 @@
+# This program adds two numbers taken as input from the user
+A=int(input("Enter a value for A: "))  # Convert input to integer
+B=int(input("Enter a value for B: "))  # Convert input to integer
+print("Value of A is:", A)
+print("Value of B is:", B)
+print("Sum of A and B is:", A + B)
